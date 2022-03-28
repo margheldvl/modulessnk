b4ebfee91bec4f8e6a44289c22fb4a0d35f3df46
Sidicrei <sidicrei.rodrigues@dvl.com.br>
2022-03-28 08:52:20 -0300

    Inserido comentário para identificar as classes/arquivos necessários para gerar o JAR.

--- ProjetoDVL/src/br/com/dvl/action/AcaoAtualizaRoteirizacaoVisita.java
+++ ProjetoDVL/src/br/com/dvl/action/AcaoAtualizaRoteirizacaoVisita.java
@@ -5,7 +5,16 @@ import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
 import br.com.sankhya.extensions.actionbutton.ContextoAcao;
 
 public class AcaoAtualizaRoteirizacaoVisita implements AcaoRotinaJava {
+	
 
+/* 
+* => ARQUIVOS NECESSÁRIOS PARA GERAR JAR 
+* 	 - C:\Projeto GitHub DVL\modulessnk\ProjetoDVL\src\br\com\dvl\action\AcaoAtualizaRoteirizacaoVisita.java
+* 	 - C:\Projeto GitHub DVL\modulessnk\ProjetoDVL\src\br\com\dvl\dao\ProgramacaoVisitaDAO.java
+* 	 - C:\Projeto GitHub DVL\modulessnk\ProjetoDVL\src\br\com\dvl\model\ProgramacaoVisita.java
+* 	 - C:\Projeto GitHub DVL\modulessnk\ProjetoDVL\src\br\com\dvl\service\ProgramacaoVisitaService.java
+* 	 - C:\Projeto GitHub DVL\modulessnk\ProjetoDVL\src\br\com\dvl\wbean\ProgramacaoVisitaWBean.java
+*/
 	public void doAction(ContextoAcao ctx) throws Exception {
 
 		StringBuffer mensagem = new StringBuffer();
